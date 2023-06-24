b805e507fb70916d4c5533125d0cc2123d862677
Seunfunmib <bamidelesheun@gmail.com>
2023-06-24 22:32:54 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+ /** @type {import('tailwindcss').Config} */
+ module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+
+      },
+      backgroundColor: {
+
+      },
+      backgroundSize:{
+        
+      },
+      backgroundImage: {
+        "overlay-img": "url('../images/icon-view.svg')",
+      },
+      textColor: {
+        "dark-cyan": "#3c8067",
+      },
+    },
+  },
+  plugins: [],
+};
